73348d7633153bbb6b5b8087171e674373440d19
Mohammad Samir Hassan <msamir.hassan1@gmail.com>
2026-01-29 19:54:34 +0000

    fixed arduino pub/sub by combining 2 pubs

--- arduino-code/micro-ros-bot-cmd-node/micro-ros-bot-cmd-node.ino
+++ arduino-code/micro-ros-bot-cmd-node/micro-ros-bot-cmd-node.ino
@@ -7,8 +7,8 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
-#include <std_msgs/msg/float32.h>
-#include <std_msgs/msg/int32.h> // NEW: For raw ticks
+#include <std_msgs/msg/int32.h>
+#include <std_msgs/msg/float32_multi_array.h> // NEW: For sending [left, right] together
 
 // --- PIN DEFINITIONS ---
 #define STBY_PIN 12
@@ -20,39 +20,47 @@
 #define PWMA_PIN 6
 #define ENC_LEFT_PIN 14
 #define ENC_RIGHT_PIN 15
+#define LED_PIN 25
 
 // --- GLOBALS ---
 rcl_publisher_t left_enc_pub;
 rcl_publisher_t right_enc_pub;
-rcl_subscription_t left_cmd_sub;
-rcl_subscription_t right_cmd_sub;
+
+// CHANGED: Single subscriber for both wheels
+rcl_subscription_t wheels_cmd_sub; 
 
 std_msgs__msg__Int32 left_enc_msg;
 std_msgs__msg__Int32 right_enc_msg;
-std_msgs__msg__Float32 left_cmd_msg;
-std_msgs__msg__Float32 right_cmd_msg;
+
+// CHANGED: MultiArray message to hold [left, right]
+std_msgs__msg__Float32MultiArray wheels_cmd_msg; 
 
 rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
 
-// Volatile for ISRs
 volatile long left_ticks = 0;
 volatile long right_ticks = 0;
 float target_left = 0.0;
 float target_right = 0.0;
 
-// --- ISRs ---
-// Note: Direction is inferred from the command because hardware is single-channel
-void isr_enc_left() {
-  if (target_left >= 0) left_ticks++;
-  else left_ticks--;
+// --- ERROR CHECK ---
+void RCCHECK(rcl_ret_t stat) {
+  if (stat != RCL_RET_OK) {
+    while (1) {
+      digitalWrite(LED_PIN, !digitalRead(LED_PIN)); // Fast blink on error
+      delay(100);
+    }
+  }
 }
 
+// --- ISRs ---
+void isr_enc_left() {
+  if (target_left >= 0) left_ticks++; else left_ticks--;
+}
 void isr_enc_right() {
-  if (target_right >= 0) right_ticks++;
-  else right_ticks--;
+  if (target_right >= 0) right_ticks++; else right_ticks--;
 }
 
 // --- MOTOR CONTROL ---
@@ -69,21 +77,24 @@ void setMotor(int pwmPin, int in1Pin, int in2Pin, float speed) {
   analogWrite(pwmPin, (int)(duty * 255));
 }
 
-// --- CALLBACKS ---
-void left_cmd_cb(const void * msgin) {
-  const std_msgs__msg__Float32 * msg = (const std_msgs__msg__Float32 *)msgin;
-  target_left = msg->data;
-  setMotor(PWMA_PIN, AIN1_PIN, AIN2_PIN, target_left);
-}
-
-void right_cmd_cb(const void * msgin) {
-  const std_msgs__msg__Float32 * msg = (const std_msgs__msg__Float32 *)msgin;
-  target_right = msg->data;
-  setMotor(PWMB_PIN, BIN1_PIN, BIN2_PIN, target_right);
+// --- CALLBACK ---
+// Expects an array of 2 floats: [left_speed, right_speed]
+void wheels_cb(const void * msgin) {
+  const std_msgs__msg__Float32MultiArray * msg = (const std_msgs__msg__Float32MultiArray *)msgin;
+  
+  // Safety check: ensure we received at least 2 numbers
+  if (msg->data.size >= 2) {
+    target_left = msg->data.data[0];
+    target_right = msg->data.data[1];
+    
+    setMotor(PWMA_PIN, AIN1_PIN, AIN2_PIN, target_left);
+    setMotor(PWMB_PIN, BIN1_PIN, BIN2_PIN, target_right);
+  }
 }
 
 // --- SETUP ---
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
   pinMode(STBY_PIN, OUTPUT);
   pinMode(PWMB_PIN, OUTPUT); pinMode(BIN2_PIN, OUTPUT); pinMode(BIN1_PIN, OUTPUT);
   pinMode(AIN1_PIN, OUTPUT); pinMode(AIN2_PIN, OUTPUT); pinMode(PWMA_PIN, OUTPUT);
@@ -96,32 +107,45 @@ void setup() {
 
   set_microros_transports();
   allocator = rcl_get_default_allocator();
-  rclc_support_init(&support, 0, NULL, &allocator);
-  rclc_node_init_default(&node, "pico_hardware_node", "", &support);
 
-  // Publishers: Raw Ticks (Int32)
-  rclc_publisher_init_default(&left_enc_pub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "left_wheel/raw_ticks");
-  rclc_publisher_init_default(&right_enc_pub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "right_wheel/raw_ticks");
+  // 1. Init Support & Node
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+  RCCHECK(rclc_node_init_default(&node, "pico_hardware_node", "", &support));
 
-  // Subscribers: Motor Commands
-  rclc_subscription_init_default(&left_cmd_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "left_motor/cmd");
-  rclc_subscription_init_default(&right_cmd_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "right_motor/cmd");
+  // 2. Init Publishers
+  RCCHECK(rclc_publisher_init_default(&left_enc_pub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "left_wheel/raw_ticks"));
+  RCCHECK(rclc_publisher_init_default(&right_enc_pub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "right_wheel/raw_ticks"));
 
-  rclc_executor_init(&executor, &support.context, 2, &allocator);
-  rclc_executor_add_subscription(&executor, &left_cmd_sub, &left_cmd_msg, &left_cmd_cb, ON_NEW_DATA);
-  rclc_executor_add_subscription(&executor, &right_cmd_sub, &right_cmd_msg, &right_cmd_cb, ON_NEW_DATA);
+  // 3. Init Subscriber (Single Topic)
+  RCCHECK(rclc_subscription_init_default(
+    &wheels_cmd_sub, 
+    &node, 
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray), 
+    "wheel_cmd"  // New Topic Name
+  ));
+  
+  // --- MEMORY ALLOCATION FOR MULTIARRAY ---
+  // We must reserve space for the incoming data array, or micro-ROS will crash.
+  // We expect 2 floats (Left, Right).
+  static float memory_buffer[2]; 
+  wheels_cmd_msg.data.capacity = 2;
+  wheels_cmd_msg.data.size = 0;
+  wheels_cmd_msg.data.data = memory_buffer;
+
+  // 4. Init Executor
+  // We now only need 3 handles (2 pubs + 1 sub)
+  RCCHECK(rclc_executor_init(&executor, &support.context, 3, &allocator));
+  RCCHECK(rclc_executor_add_subscription(&executor, &wheels_cmd_sub, &wheels_cmd_msg, &wheels_cb, ON_NEW_DATA));
 }
 
 // --- LOOP ---
 void loop() {
-  // Populate Messages
   left_enc_msg.data = (int32_t)left_ticks;
   right_enc_msg.data = (int32_t)right_ticks;
 
-  // Publish
   rcl_publish(&left_enc_pub, &left_enc_msg, NULL);
   rcl_publish(&right_enc_pub, &right_enc_msg, NULL);
 
   rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10));
-  delay(50); // Publish at ~20Hz
+  delay(50);
 }
--- src/rosbot-wheel-comm.cpp
+++ src/rosbot-wheel-comm.cpp
@@ -2,64 +2,90 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/int32.hpp"
+#include "std_msgs/msg/float32_multi_array.hpp" // Required for the array command
 
 using namespace std::chrono_literals;
-const double wheel_dia = 0.04;
-const double wheel_separation = 0.16;
-const double pulses_per_revolution = 200.0;
+using std::placeholders::_1;
 
-class MinimalPublisher : public rclcpp::Node
+class WheelController : public rclcpp::Node
 {
 public:
-  MinimalPublisher()
-  : Node("minimal_publisher"), count_(0)
+  WheelController()
+  : Node("wheel_controller_node"), left_ticks_(0), right_ticks_(0)
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    // --- SUBSCRIBERS (Listen to the Pico) ---
+    // Subscribe to Left Encoder
+    left_sub_ = this->create_subscription<std_msgs::msg::Int32>(
+      "left_wheel/raw_ticks", 10,
+      std::bind(&WheelController::left_callback, this, _1));
+
+    // Subscribe to Right Encoder
+    right_sub_ = this->create_subscription<std_msgs::msg::Int32>(
+      "right_wheel/raw_ticks", 10,
+      std::bind(&WheelController::right_callback, this, _1));
+
+    // --- PUBLISHER (Talk to the Pico) ---
+    // Publisher for motor commands [left_speed, right_speed]
+    cmd_pub_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("wheel_cmd", 10);
+
+    // --- TIMER (Control Loop) ---
+    // Every 500ms, send a command and print status
     timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      500ms, std::bind(&WheelController::timer_callback, this));
+      
+    RCLCPP_INFO(this->get_logger(), "Wheel Controller Started. Sending commands...");
   }
 
 private:
+  // Update internal state when left ticks arrive
+  void left_callback(const std_msgs::msg::Int32::SharedPtr msg)
+  {
+    left_ticks_ = msg->data;
+  }
+
+  // Update internal state when right ticks arrive
+  void right_callback(const std_msgs::msg::Int32::SharedPtr msg)
+  {
+    right_ticks_ = msg->data;
+  }
+
+  // Main loop: Send motor commands and log data
   void timer_callback()
   {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
+    // 1. Create the message
+    auto message = std_msgs::msg::Float32MultiArray();
+    
+    // 2. Set the data: [Left Speed, Right Speed]
+    // Sending 0.5 (50% speed) to both motors
+    message.data = {0.5, 0.5}; 
+
+    // 3. Publish the command
+    cmd_pub_->publish(message);
+
+    // 4. Log the feedback
+    RCLCPP_INFO(this->get_logger(), 
+      "Sent: [0.5, 0.5] | Encoders - Left: %d, Right: %d", 
+      left_ticks_, right_ticks_);
   }
+
+  // Member variables
+  rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr left_sub_;
+  rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr right_sub_;
+  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr cmd_pub_;
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
-};
 
-
-class MinimalSubscriber : public rclcpp::Node
-{
-public:
-  MinimalSubscriber()
-  : Node("left_wheel_subscriber")
-  {
-    subscription_ = this->create_subscription<std_msgs::msg::Int32>(
-      "left_wheel/raw_ticks", 10,
-      std::bind(&MinimalSubscriber::topic_callback, this, std::placeholders::_1));
-  }
-private:
-  void topic_callback(const std_msgs::msg::Int32::SharedPtr msg) const
-  {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%d'", msg->data);
-  }
-  rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr subscription_;
+  int32_t left_ticks_;
+  int32_t right_ticks_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  // rclcpp::spin(std::make_shared<MinimalPublisher>());
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::spin(std::make_shared<WheelController>());
   rclcpp::shutdown();
   return 0;
 }
